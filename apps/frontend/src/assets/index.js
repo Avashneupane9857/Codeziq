7f666f2640d9ff5757085ac39b14b135bd8b0d9b
Siddhanta Acharya <siddhu12980@gmail.com>
2024-10-02 17:46:18 +0530

    :building_construction: added turborepo

---
+++ apps/frontend/src/assets/index.js
@@ -0,0 +1,4 @@
+import logo from "./Logo.png"
+import python from "./python.png"
+import js from "./js.png"
+export { logo, python, js };
--- frontend/src/assets/index.js
+++
@@ -1,4 +0,0 @@
-import logo from "./logo.png";
-import js from "./js.png";
-import python from "./python.png";
-export { logo, python, js };
